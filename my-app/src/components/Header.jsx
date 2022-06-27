f27cbedaedbe1eb207eb697c04a6736a5597d27b
MF249 <mfaizel@knights.ucf.edu>
2022-06-27 18:28:03 -0400

    added header to main page

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header-div">
+                <h2 style={{ color: "white" }}>Live Bolt Smart Lock</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
